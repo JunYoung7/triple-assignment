61c826595a2aa8912c7adae0b7666f1b37955477
JunYoung <vnslt0152@gmail.com>
2022-06-22 17:06:29 +0900

    fix : fix .eslintrc.js for 'cr' error
    - add rules to .eslintrc.js file for error solving (delete 'cr' eslint(prettier/prettier)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend', project: './tsconfig.json' })
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'node',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [...extendConfigs],
+  overrides: [...overrides],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
